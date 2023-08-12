cf43928888c3da7b2ecc0e8f36cec41b904ec277
luis ivetta <114588@tecnicatura.frc.utn.edu.ar>
2023-08-12 10:14:11 -0300

    casi terminado

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,60 +28,65 @@
         /// </summary>
         private void InitializeComponent()
         {
-            panel1 = new Panel();
-            lblTiempo1 = new Label();
-            label1 = new Label();
-            lblSegundos1 = new Label();
+            components = new System.ComponentModel.Container();
+            panel3 = new Panel();
+            lblDia1 = new Label();
             lblFecha1 = new Label();
+            lblSegundo1 = new Label();
+            lblHora1 = new Label();
+            label12 = new Label();
+            panel1 = new Panel();
+            lblDia2 = new Label();
+            lblFecha2 = new Label();
+            lblSegundo2 = new Label();
+            lblHora2 = new Label();
+            label5 = new Label();
+            panel2 = new Panel();
+            lblDia3 = new Label();
+            lblFecha3 = new Label();
+            lblSegundo3 = new Label();
+            lblHora3 = new Label();
+            label6 = new Label();
+            panel4 = new Panel();
+            lblDia4 = new Label();
+            lblFecha4 = new Label();
+            lblSegundo4 = new Label();
+            lblHora4 = new Label();
+            label7 = new Label();
+            timer1 = new System.Windows.Forms.Timer(components);
+            timer2 = new System.Windows.Forms.Timer(components);
+            timer3 = new System.Windows.Forms.Timer(components);
+            timer4 = new System.Windows.Forms.Timer(components);
+            panel3.SuspendLayout();
             panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            panel4.SuspendLayout();
             SuspendLayout();
             // 
-            // panel1
+            // panel3
             // 
-            panel1.BorderStyle = BorderStyle.Fixed3D;
-            panel1.Controls.Add(lblFecha1);
-            panel1.Controls.Add(lblSegundos1);
-            panel1.Controls.Add(lblTiempo1);
-            panel1.Controls.Add(label1);
-            panel1.Location = new Point(75, 44);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(324, 163);
-            panel1.TabIndex = 0;
+            panel3.BorderStyle = BorderStyle.Fixed3D;
+            panel3.Controls.Add(lblDia1);
+            panel3.Controls.Add(lblFecha1);
+            panel3.Controls.Add(lblSegundo1);
+            panel3.Controls.Add(lblHora1);
+            panel3.Controls.Add(label12);
+            panel3.Location = new Point(75, 34);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(416, 172);
+            panel3.TabIndex = 1;
             // 
-            // lblTiempo1
+            // lblDia1
             // 
-            lblTiempo1.AutoSize = true;
-            lblTiempo1.BorderStyle = BorderStyle.FixedSingle;
-            lblTiempo1.Font = new Font("DS-Digital", 36F, FontStyle.Regular, GraphicsUnit.Point);
-            lblTiempo1.ForeColor = SystemColors.HighlightText;
-            lblTiempo1.Location = new Point(109, 39);
-            lblTiempo1.Name = "lblTiempo1";
-            lblTiempo1.Size = new Size(118, 49);
-            lblTiempo1.TabIndex = 1;
-            lblTiempo1.Text = "13:00";
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            label1.ForeColor = SystemColors.HighlightText;
-            label1.Location = new Point(109, 0);
-            label1.Name = "label1";
-            label1.Size = new Size(84, 21);
-            label1.TabIndex = 0;
-            label1.Text = "Hora Local";
-            // 
-            // lblSegundos1
-            // 
-            lblSegundos1.AutoSize = true;
-            lblSegundos1.BorderStyle = BorderStyle.FixedSingle;
-            lblSegundos1.Font = new Font("DS-Digital", 24F, FontStyle.Regular, GraphicsUnit.Point);
-            lblSegundos1.ForeColor = SystemColors.HighlightText;
-            lblSegundos1.Location = new Point(216, 39);
-            lblSegundos1.Name = "lblSegundos1";
-            lblSegundos1.Size = new Size(48, 33);
-            lblSegundos1.TabIndex = 2;
-            lblSegundos1.Text = "43";
+            lblDia1.AutoSize = true;
+            lblDia1.BorderStyle = BorderStyle.FixedSingle;
+            lblDia1.Font = new Font("DS-Digital", 26.25F, FontStyle.Regular, GraphicsUnit.Point);
+            lblDia1.ForeColor = SystemColors.HighlightText;
+            lblDia1.Location = new Point(269, 106);
+            lblDia1.Name = "lblDia1";
+            lblDia1.Size = new Size(119, 37);
+            lblDia1.TabIndex = 4;
+            lblDia1.Text = "Sabado";
             // 
             // lblFecha1
             // 
@@ -89,32 +94,338 @@
             lblFecha1.BorderStyle = BorderStyle.FixedSingle;
             lblFecha1.Font = new Font("DS-Digital", 26.25F, FontStyle.Regular, GraphicsUnit.Point);
             lblFecha1.ForeColor = SystemColors.HighlightText;
-            lblFecha1.Location = new Point(35, 104);
+            lblFecha1.Location = new Point(17, 106);
             lblFecha1.Name = "lblFecha1";
-            lblFecha1.Size = new Size(229, 37);
+            lblFecha1.Size = new Size(153, 37);
             lblFecha1.TabIndex = 3;
-            lblFecha1.Text = "12 Agosto 2023";
+            lblFecha1.Text = "11 Ao 2023";
+            // 
+            // lblSegundo1
+            // 
+            lblSegundo1.AutoSize = true;
+            lblSegundo1.BorderStyle = BorderStyle.FixedSingle;
+            lblSegundo1.Font = new Font("DS-Digital", 24F, FontStyle.Regular, GraphicsUnit.Point);
+            lblSegundo1.ForeColor = SystemColors.HighlightText;
+            lblSegundo1.Location = new Point(234, 36);
+            lblSegundo1.Name = "lblSegundo1";
+            lblSegundo1.Size = new Size(48, 33);
+            lblSegundo1.TabIndex = 2;
+            lblSegundo1.Text = "43";
+            // 
+            // lblHora1
+            // 
+            lblHora1.AutoSize = true;
+            lblHora1.BorderStyle = BorderStyle.FixedSingle;
+            lblHora1.Font = new Font("DS-Digital", 36F, FontStyle.Regular, GraphicsUnit.Point);
+            lblHora1.ForeColor = SystemColors.HighlightText;
+            lblHora1.Location = new Point(110, 34);
+            lblHora1.Name = "lblHora1";
+            lblHora1.Size = new Size(118, 49);
+            lblHora1.TabIndex = 1;
+            lblHora1.Text = "13:00";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label12.ForeColor = SystemColors.HighlightText;
+            label12.Location = new Point(162, 0);
+            label12.Name = "label12";
+            label12.Size = new Size(84, 21);
+            label12.TabIndex = 0;
+            label12.Text = "Hora Local";
+            // 
+            // panel1
+            // 
+            panel1.BorderStyle = BorderStyle.Fixed3D;
+            panel1.Controls.Add(lblDia2);
+            panel1.Controls.Add(lblFecha2);
+            panel1.Controls.Add(lblSegundo2);
+            panel1.Controls.Add(lblHora2);
+            panel1.Controls.Add(label5);
+            panel1.Location = new Point(521, 36);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(416, 172);
+            panel1.TabIndex = 5;
+            // 
+            // lblDia2
+            // 
+            lblDia2.AutoSize = true;
+            lblDia2.BorderStyle = BorderStyle.FixedSingle;
+            lblDia2.Font = new Font("DS-Digital", 26.25F, FontStyle.Regular, GraphicsUnit.Point);
+            lblDia2.ForeColor = SystemColors.HighlightText;
+            lblDia2.Location = new Point(269, 106);
+            lblDia2.Name = "lblDia2";
+            lblDia2.Size = new Size(119, 37);
+            lblDia2.TabIndex = 4;
+            lblDia2.Text = "Sabado";
+            // 
+            // lblFecha2
+            // 
+            lblFecha2.AutoSize = true;
+            lblFecha2.BorderStyle = BorderStyle.FixedSingle;
+            lblFecha2.Font = new Font("DS-Digital", 26.25F, FontStyle.Regular, GraphicsUnit.Point);
+            lblFecha2.ForeColor = SystemColors.HighlightText;
+            lblFecha2.Location = new Point(17, 106);
+            lblFecha2.Name = "lblFecha2";
+            lblFecha2.Size = new Size(162, 37);
+            lblFecha2.TabIndex = 3;
+            lblFecha2.Text = "11 1to 2023";
+            // 
+            // lblSegundo2
+            // 
+            lblSegundo2.AutoSize = true;
+            lblSegundo2.BorderStyle = BorderStyle.FixedSingle;
+            lblSegundo2.Font = new Font("DS-Digital", 24F, FontStyle.Regular, GraphicsUnit.Point);
+            lblSegundo2.ForeColor = SystemColors.HighlightText;
+            lblSegundo2.Location = new Point(234, 34);
+            lblSegundo2.Name = "lblSegundo2";
+            lblSegundo2.Size = new Size(48, 33);
+            lblSegundo2.TabIndex = 2;
+            lblSegundo2.Text = "43";
+            // 
+            // lblHora2
+            // 
+            lblHora2.AutoSize = true;
+            lblHora2.BorderStyle = BorderStyle.FixedSingle;
+            lblHora2.Font = new Font("DS-Digital", 36F, FontStyle.Regular, GraphicsUnit.Point);
+            lblHora2.ForeColor = SystemColors.HighlightText;
+            lblHora2.Location = new Point(110, 34);
+            lblHora2.Name = "lblHora2";
+            lblHora2.Size = new Size(118, 49);
+            lblHora2.TabIndex = 1;
+            lblHora2.Text = "13:00";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.ForeColor = SystemColors.HighlightText;
+            label5.Location = new Point(162, 0);
+            label5.Name = "label5";
+            label5.Size = new Size(59, 21);
+            label5.TabIndex = 0;
+            label5.Text = "España";
+            // 
+            // panel2
+            // 
+            panel2.BorderStyle = BorderStyle.Fixed3D;
+            panel2.Controls.Add(lblDia3);
+            panel2.Controls.Add(lblFecha3);
+            panel2.Controls.Add(lblSegundo3);
+            panel2.Controls.Add(lblHora3);
+            panel2.Controls.Add(label6);
+            panel2.Location = new Point(75, 248);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(416, 172);
+            panel2.TabIndex = 5;
+            // 
+            // lblDia3
+            // 
+            lblDia3.AutoSize = true;
+            lblDia3.BorderStyle = BorderStyle.FixedSingle;
+            lblDia3.Font = new Font("DS-Digital", 26.25F, FontStyle.Regular, GraphicsUnit.Point);
+            lblDia3.ForeColor = SystemColors.HighlightText;
+            lblDia3.Location = new Point(269, 106);
+            lblDia3.Name = "lblDia3";
+            lblDia3.Size = new Size(119, 37);
+            lblDia3.TabIndex = 4;
+            lblDia3.Text = "Sabado";
+            // 
+            // lblFecha3
+            // 
+            lblFecha3.AutoSize = true;
+            lblFecha3.BorderStyle = BorderStyle.FixedSingle;
+            lblFecha3.Font = new Font("DS-Digital", 26.25F, FontStyle.Regular, GraphicsUnit.Point);
+            lblFecha3.ForeColor = SystemColors.HighlightText;
+            lblFecha3.Location = new Point(17, 106);
+            lblFecha3.Name = "lblFecha3";
+            lblFecha3.Size = new Size(169, 37);
+            lblFecha3.TabIndex = 3;
+            lblFecha3.Text = "34 to 2023";
+            // 
+            // lblSegundo3
+            // 
+            lblSegundo3.AutoSize = true;
+            lblSegundo3.BorderStyle = BorderStyle.FixedSingle;
+            lblSegundo3.Font = new Font("DS-Digital", 24F, FontStyle.Regular, GraphicsUnit.Point);
+            lblSegundo3.ForeColor = SystemColors.HighlightText;
+            lblSegundo3.Location = new Point(234, 34);
+            lblSegundo3.Name = "lblSegundo3";
+            lblSegundo3.Size = new Size(48, 33);
+            lblSegundo3.TabIndex = 2;
+            lblSegundo3.Text = "43";
+            // 
+            // lblHora3
+            // 
+            lblHora3.AutoSize = true;
+            lblHora3.BorderStyle = BorderStyle.FixedSingle;
+            lblHora3.Font = new Font("DS-Digital", 36F, FontStyle.Regular, GraphicsUnit.Point);
+            lblHora3.ForeColor = SystemColors.HighlightText;
+            lblHora3.Location = new Point(110, 34);
+            lblHora3.Name = "lblHora3";
+            lblHora3.Size = new Size(118, 49);
+            lblHora3.TabIndex = 1;
+            lblHora3.Text = "13:00";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label6.ForeColor = SystemColors.HighlightText;
+            label6.Location = new Point(162, 0);
+            label6.Name = "label6";
+            label6.Size = new Size(75, 21);
+            label6.TabIndex = 0;
+            label6.Text = "Alemania";
+            // 
+            // panel4
+            // 
+            panel4.BorderStyle = BorderStyle.Fixed3D;
+            panel4.Controls.Add(lblDia4);
+            panel4.Controls.Add(lblFecha4);
+            panel4.Controls.Add(lblSegundo4);
+            panel4.Controls.Add(lblHora4);
+            panel4.Controls.Add(label7);
+            panel4.Location = new Point(521, 248);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(416, 172);
+            panel4.TabIndex = 6;
+            // 
+            // lblDia4
+            // 
+            lblDia4.AutoSize = true;
+            lblDia4.BorderStyle = BorderStyle.FixedSingle;
+            lblDia4.Font = new Font("DS-Digital", 26.25F, FontStyle.Regular, GraphicsUnit.Point);
+            lblDia4.ForeColor = SystemColors.HighlightText;
+            lblDia4.Location = new Point(269, 106);
+            lblDia4.Name = "lblDia4";
+            lblDia4.Size = new Size(119, 37);
+            lblDia4.TabIndex = 4;
+            lblDia4.Text = "Sabado";
+            // 
+            // lblFecha4
+            // 
+            lblFecha4.AutoSize = true;
+            lblFecha4.BorderStyle = BorderStyle.FixedSingle;
+            lblFecha4.Font = new Font("DS-Digital", 26.25F, FontStyle.Regular, GraphicsUnit.Point);
+            lblFecha4.ForeColor = SystemColors.HighlightText;
+            lblFecha4.Location = new Point(17, 106);
+            lblFecha4.Name = "lblFecha4";
+            lblFecha4.Size = new Size(144, 37);
+            lblFecha4.TabIndex = 3;
+            lblFecha4.Text = "1 Ag 2023";
+            // 
+            // lblSegundo4
+            // 
+            lblSegundo4.AutoSize = true;
+            lblSegundo4.BorderStyle = BorderStyle.FixedSingle;
+            lblSegundo4.Font = new Font("DS-Digital", 24F, FontStyle.Regular, GraphicsUnit.Point);
+            lblSegundo4.ForeColor = SystemColors.HighlightText;
+            lblSegundo4.Location = new Point(234, 34);
+            lblSegundo4.Name = "lblSegundo4";
+            lblSegundo4.Size = new Size(48, 33);
+            lblSegundo4.TabIndex = 2;
+            lblSegundo4.Text = "43";
+            // 
+            // lblHora4
+            // 
+            lblHora4.AutoSize = true;
+            lblHora4.BorderStyle = BorderStyle.FixedSingle;
+            lblHora4.Font = new Font("DS-Digital", 36F, FontStyle.Regular, GraphicsUnit.Point);
+            lblHora4.ForeColor = SystemColors.HighlightText;
+            lblHora4.Location = new Point(110, 34);
+            lblHora4.Name = "lblHora4";
+            lblHora4.Size = new Size(118, 49);
+            lblHora4.TabIndex = 1;
+            lblHora4.Text = "13:00";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label7.ForeColor = SystemColors.HighlightText;
+            label7.Location = new Point(162, 0);
+            label7.Name = "label7";
+            label7.Size = new Size(71, 21);
+            label7.TabIndex = 0;
+            label7.Text = "Noruega";
+            // 
+            // timer1
+            // 
+            timer1.Tick += timer1_Tick;
+            // 
+            // timer2
+            // 
+            timer2.Tick += timer2_Tick;
+            // 
+            // timer3
+            // 
+            timer3.Tick += timer3_Tick;
+            // 
+            // timer4
+            // 
+            timer4.Tick += timer4_Tick;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.Desktop;
-            ClientSize = new Size(1056, 475);
+            ClientSize = new Size(1017, 475);
+            Controls.Add(panel4);
+            Controls.Add(panel2);
             Controls.Add(panel1);
+            Controls.Add(panel3);
             Name = "Form1";
             Text = "Reloj";
+            Load += form1_Load;
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            panel4.ResumeLayout(false);
+            panel4.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
-
-        private Panel panel1;
         private Label lblTiempo1;
         private Label label1;
         private Label lblSegundos1;
         private Label lblFecha1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Panel panel3;
+        private Label lblDia1;
+        private Label label9;
+        private Label lblSegundo1;
+        private Label lblHora1;
+        private Label label12;
+        private Panel panel1;
+        private Label lblDia2;
+        private Label lblFecha2;
+        private Label lblSegundo2;
+        private Label lblHora2;
+        private Label label5;
+        private Panel panel2;
+        private Label lblDia3;
+        private Label lblFecha3;
+        private Label lblSegundo3;
+        private Label lblHora3;
+        private Label label6;
+        private Panel panel4;
+        private Label lblDia4;
+        private Label lblFecha4;
+        private Label lblSegundo4;
+        private Label lblHora4;
+        private Label label7;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Timer timer4;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,55 @@ namespace Reloj
         {
             InitializeComponent();
         }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblHora1.Text = DateTime.Now.ToString("hh:mm");
+            lblSegundo1.Text = DateTime.Now.ToString("ss");
+            lblFecha1.Text = DateTime.Now.ToString("dd MMMM yyyy");
+            lblDia1.Text = DateTime.Now.ToString("dddd");
+        }
+
+        private void form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+            timer2.Start();
+            timer3.Start();
+            timer4.Start();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            var spainTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Romance Standard Time");
+            var espana = TimeZoneInfo.ConvertTime(DateTime.Now, spainTimeZone);
+
+            lblHora2.Text = espana.ToString("hh:mm");
+            lblSegundo2.Text = espana.ToString("ss");
+            lblFecha2.Text = espana.ToString("dd MMMM yyyy");
+            lblDia2.Text = espana.ToString("dddd");
+
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            var alemaniaTimeZone = TimeZoneInfo.FindSystemTimeZoneById("W. Europe Standard Time");
+            var alemania = TimeZoneInfo.ConvertTime(DateTime.Now, alemaniaTimeZone);
+
+            lblHora3.Text = alemania.ToString("hh:mm");
+            lblSegundo3.Text = alemania.ToString("ss");
+            lblFecha3.Text = alemania.ToString("dd MMMM yyyy");
+            lblDia3.Text = alemania.ToString("dddd");
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            var norwayTimeZone = TimeZoneInfo.FindSystemTimeZoneById("FLE Standard Time");
+            var noruega = TimeZoneInfo.ConvertTime(DateTime.Now, norwayTimeZone);
+
+            lblHora4.Text = noruega.ToString("hh:mm");
+            lblSegundo4.Text = noruega.ToString("ss");
+            lblFecha4.Text = noruega.ToString("dd MMMM yyyy");
+            lblDia4.Text = noruega.ToString("dddd");
+        }
     }
 }
